df2f1e3a91eafd08bb76a09bbafa15928d0527f9
bot209 <bot2014cs@gmail.com>
2023-04-13 00:01:27 +0300

    Отрефакторить комментарии к коду

--- tests/test_find_pet.py
+++ tests/test_find_pet.py
@@ -1,7 +1,9 @@
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertion import Assertion
+import pytest
 
+@pytest.mark.GET_methods
 class TestFindPet(BaseCase):
     def setup_method(self):
         self.petId = 5
@@ -9,23 +11,23 @@ class TestFindPet(BaseCase):
         self.pet_id = self.get_json(response, 'id')
         self.pet_name = self.get_json(response, 'name')
 
-# 1. Проверить получение правильного статус кода
+# 1. Проверка получения правильного статус кода
     def test_get_valid_status_code(self):
         response = MyRequests.get(f'/pet/{self.petId}')
         Assertion.assert_code_status(response, 200)
 
-# 2. Проверить что нам приходят значения id, name питомца
+# 2. Проверка что значения id, name питомца возвращаются корректно
     def test_get_valid_pet_id_and_name(self):
         response = MyRequests.get(f'/pet/{self.petId}')
         Assertion.assert_json_has_key(response, 'id')
         Assertion.assert_json_has_key(response, 'name')  
 
-# 3. Проверить ввод некорректного Id питомца
+# 3. Проверка некорректных данных: petId
     def test_get_no_valid_pet_id(self):
         response = MyRequests.get('/pet/2222')
         Assertion.assert_code_status(response, 404)
 
-# 4. Проверить ввод пустого значения id питомца
+# 4. Отправление пустого запроса
     def test_get_not_valid_params(self):
         response = MyRequests.get('/pet/')
         Assertion.assert_code_status(response, 405)
--- tests/test_update_pet.py
+++ tests/test_update_pet.py
@@ -1,27 +1,30 @@
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertion import Assertion
-import requests
+import pytest
 
+@pytest.mark.POST_methods
 class TestUpdatePet(BaseCase):
     def setup_method(self):
         self.petId = 5
-        self.name = 'test228'
+        self.name = 'Test123'
         self.status = '123'
         self.data = {'petId': self.petId, 'name': self.name, 'status': self.status}
         
-# 1. Изменить значения питомца
+# 1. Измение значения питомца
     def test_check_input_of_new_value(self):
         response1 = MyRequests.post(f'/pet/{self.petId}', data=self.data)
         Assertion.assert_code_status(response1, 200)
         
-# 2. Проверить что данные изменились
+# 2. Проверка изменения данных: petId, name, status
     def test_check_changed_value(self):
         response2 = MyRequests.get(f'/pet/{self.petId}')
+        Assertion.assert_code_status(response2, 200)
+        Assertion.assert_json_value_by_name(response2, 'id', self.petId, 'Айдишник не сходятся')
         Assertion.assert_json_value_by_name(response2, 'name', self.name, 'Имена не сходятся')
         Assertion.assert_json_value_by_name(response2, 'status', self.status, 'Статусы не сходятся')
 
-# 3. Проверить ввод некорректных данных
+# 3. Проверка некорректных данных: petId, name, status
     def test_check_input_of_icorrect_value(self):
         petId = 'name'
         name = 123
@@ -30,7 +33,7 @@ class TestUpdatePet(BaseCase):
         response = MyRequests.post(f'/pet/{petId}', data=data)
         Assertion.assert_code_status(response, 404)
 
-# 4. Проверить ввод пустых данных
+# 4. Отправление пустого запроса
     def test_check_for_empty_values(self):
         petId = ''
         name = ''
